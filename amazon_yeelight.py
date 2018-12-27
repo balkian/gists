eaad409a8a963969e4ce44156be36fac31972622
J. Fernando Sánchez <balkian@gmail.com>
2018-12-27 21:44:07 +0100

    Simplify toggle

--- amazon_yeelight.py
+++ amazon_yeelight.py
@@ -1,9 +1,9 @@
+#!/bin/env python
 from scapy.all import *
 from yeelight import Bulb
 
 MAC_ADDRESS = 'ac:63:be:52:8b:4a' # enter Dash Button's MAC Address here.
 
-on = False
 b = Bulb('192.168.2.240')
 
 def detect_button(pkt):
@@ -11,11 +11,6 @@ def detect_button(pkt):
     if pkt.haslayer(DHCP) and pkt[Ether].src == MAC_ADDRESS:
         global on
         print("Button Press Detected")
-        if on:
-            b.turn_off()
-        else:
-            b.turn_on()
-        on = on ^ True
-        #Do stuff
+        b.toggle()
 
 sniff(prn=detect_button, filter="(udp and (port 67 or 68))", store=0)
